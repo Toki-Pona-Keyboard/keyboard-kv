c96bd383950a1344c321c4ec265f27ea8ebeac0f
BoolmanO <96051241+BoolmanO@users.noreply.github.com>
2024-02-26 06:08:06 +0600

    Some changes
    some changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from kivy.uix.button import Button
 
 class TokiPonaKeyboardApp(App):
     def build(self):
-        return Button(text="hello tokipona")
+        return Button(text="hello tokipona!!!")
 
 if __name__ == "__main__":
     TokiPonaKeyboardApp().run()
